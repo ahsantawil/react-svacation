094801451199cda81aa2207a13524b811ebe0dcd
ahsantawil <ahsan.tawil07@gmail.com>
2020-09-02 22:35:52 +0700

    button element test

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from 'elements/Button/index';
+
+import 'assets/scss/style.scss';
+
+function App() {
+  return (
+    <div className="App">
+      Hai Dunia !
+      <Button />
+    </div>
+  );
+}
+
+export default App;
