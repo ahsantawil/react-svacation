8fddabd80e54123a899491ab6ec7f08ea7d1dd77
ahsantawil <ahsan.tawil07@gmail.com>
2020-09-05 23:03:59 +0700

    add file in landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import Headers from 'parts/Headers';
+import Hero from 'parts/Hero';
+import MostPicked from 'parts/MostPicked';
+
+import landingPage from 'json/landingPage.json';
 
 export default class LandingPage extends Component {
     render() {
         return (
             <>
                 <Headers {...this.props}></Headers>
+                <Hero data={landingPage.hero} />
+                <MostPicked data={landingPage.mostPicked} />
             </>
         );
     }
